5e8864164bc4c9ff0d77782e5b87baa6f7e7d974
zotikova <zotikovadana@gmail.com>
2020-12-11 20:23:06 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+a = 1
+b = 2
+c = a + b
+
+
+# print(c)
+
+
+def testFunction():
+    for i in range(1000):
+        for g in range(5):
+            time.sleep(1)
+            print(i)
+
+
+testFunction()
